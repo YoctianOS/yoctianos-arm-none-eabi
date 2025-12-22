5f682d3bc0fe5d2d2504d0bdc3caf0b3decd0b12
AstromanGaming <astromangamingofficial@gmail.com>
2025-12-22 08:37:59 -0500

    Fix

--- yoctianos_crosstool-ng.sh
+++ yoctianos_crosstool-ng.sh
@@ -2,15 +2,15 @@
 
 BASE_DIR="$HOME/yoctianos-arm-none-eabi"
 
-echo "0. Download DEB packages (if it's nessesary)"
-sudo apt update
-sudo apt install -y git build-essential gperf bison flex texinfo libtool libncurses5-dev python3 gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf binutils-arm-linux-gnueabihf help2man
-
-echo "1. GIT pull the yoctianos-arm-none-eabi (if it's nessesary)"
+echo "0. GIT pull the yoctianos-arm-none-eabi (if it's nessesary)"
 cd $BASE_DIR
 git pull
 
-echo "2a. GIT clone the crosstool-ng"
+echo "1. Download DEB packages (if it's nessesary)"
+sudo apt update
+sudo apt install -y git build-essential gperf bison flex texinfo libtool libncurses5-dev python3 gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf binutils-arm-linux-gnueabihf help2man
+
+echo "2a. GIT clone the crosstool-ng (if it's nessesary)"
 cd $HOME
 git clone https://github.com/crosstool-ng/crosstool-ng.git
 
